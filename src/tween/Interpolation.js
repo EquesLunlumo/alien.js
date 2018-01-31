5cd812d2a434366ba31193c6343a52e32681afc8
Patrick Schroen <ps@ufotechnologies.com>
2018-01-31 16:58:04 -0500

    Interpolation.Bounce fix

--- src/tween/Interpolation.js
+++ src/tween/Interpolation.js
@@ -259,7 +259,7 @@ class Interpolation {
 
         this.Bounce = {
             In(k) {
-                return 1 - this.Bounce.Out(1 - k);
+                return 1 - Interpolation.Bounce.Out(1 - k);
             },
             Out(k) {
                 if (k < 1 / 2.75) return 7.5625 * k * k;
@@ -268,8 +268,8 @@ class Interpolation {
                 return 7.5625 * (k -= 2.625 / 2.75) * k + 0.984375;
             },
             InOut(k) {
-                if (k < 0.5) return this.Bounce.In(k * 2) * 0.5;
-                return this.Bounce.Out(k * 2 - 1) * 0.5 + 0.5;
+                if (k < 0.5) return Interpolation.Bounce.In(k * 2) * 0.5;
+                return Interpolation.Bounce.Out(k * 2 - 1) * 0.5 + 0.5;
             }
         };
     }
