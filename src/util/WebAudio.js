24bb0675f487a68dd1901c4e1d7a8ffa787f48db
Patrick Schroen <ps@ufotechnologies.com>
2018-03-13 22:06:57 -0400

    Let WebAudio init finish

--- src/util/WebAudio.js
+++ src/util/WebAudio.js
@@ -13,27 +13,30 @@ if (!window.AudioContext) window.AudioContext = window.webkitAudioContext || win
 class WebAudio {
 
     static init() {
-        if (!window.AudioContext) return;
 
         if (!this.active) {
             this.active = true;
 
             const self = this;
             const sounds = {};
+            let context;
 
-            this.context = new AudioContext();
-            this.globalGain = this.context.createGain();
-            this.globalGain.connect(this.context.destination);
-            this.globalGain.value = this.globalGain.gain.defaultValue;
-            this.gain = {
-                set value(value) {
-                    self.globalGain.value = value;
-                    self.globalGain.gain.setTargetAtTime(value, self.context.currentTime, 0.01);
-                },
-                get value() {
-                    return self.globalGain.value;
-                }
-            };
+            if (window.AudioContext) {
+                context = new AudioContext();
+                this.globalGain = context.createGain();
+                this.globalGain.connect(context.destination);
+                this.globalGain.value = this.globalGain.gain.defaultValue;
+                this.gain = {
+                    set value(value) {
+                        self.globalGain.value = value;
+                        self.globalGain.gain.setTargetAtTime(value, context.currentTime, 0.01);
+                    },
+                    get value() {
+                        return self.globalGain.value;
+                    }
+                };
+                this.context = context;
+            }
 
             this.loadSound = (id, callback) => {
                 const promise = Promise.create();
@@ -43,7 +46,7 @@ class WebAudio {
                 window.fetch(sound.asset).then(response => {
                     if (!response.ok) return callback();
                     response.arrayBuffer().then(data => {
-                        this.context.decodeAudioData(data, buffer => {
+                        context.decodeAudioData(data, buffer => {
                             sound.buffer = buffer;
                             sound.complete = true;
                             callback();
@@ -58,13 +61,13 @@ class WebAudio {
             this.createSound = (id, asset, callback) => {
                 const sound = {};
                 sound.asset = Assets.getPath(asset);
-                sound.audioGain = this.context.createGain();
+                sound.audioGain = context.createGain();
                 sound.audioGain.connect(this.globalGain);
                 sound.audioGain.value = sound.audioGain.gain.defaultValue;
                 sound.gain = {
                     set value(value) {
                         sound.audioGain.value = value;
-                        sound.audioGain.gain.setTargetAtTime(value, self.context.currentTime, 0.01);
+                        sound.audioGain.gain.setTargetAtTime(value, context.currentTime, 0.01);
                     },
                     get value() {
                         return sound.audioGain.value;
@@ -83,15 +86,16 @@ class WebAudio {
             };
 
             this.trigger = id => {
-                if (this.context.state === 'suspended') this.context.resume();
+                if (!context) return;
+                if (context.state === 'suspended') context.resume();
                 const sound = this.getSound(id);
                 if (!sound.ready) this.loadSound(id);
                 sound.ready().then(() => {
                     if (sound.complete) {
-                        sound.source = this.context.createBufferSource();
+                        sound.source = context.createBufferSource();
                         sound.source.buffer = sound.buffer;
                         sound.source.connect(sound.audioGain);
-                        sound.audioGain.gain.setValueAtTime(sound.audioGain.value, this.context.currentTime);
+                        sound.audioGain.gain.setValueAtTime(sound.audioGain.value, context.currentTime);
                         sound.source.loop = !!sound.loop;
                         sound.source.start();
                     }
@@ -99,6 +103,7 @@ class WebAudio {
             };
 
             this.play = (id, volume = 1, loop) => {
+                if (!context) return;
                 if (typeof volume === 'boolean') {
                     loop = volume;
                     volume = 1;
@@ -112,6 +117,7 @@ class WebAudio {
             };
 
             this.fadeInAndPlay = (id, volume, loop, time, ease, delay = 0) => {
+                if (!context) return;
                 const sound = this.getSound(id);
                 if (sound) {
                     sound.gain.value = 0;
@@ -122,19 +128,23 @@ class WebAudio {
             };
 
             this.fadeOutAndStop = (id, time, ease, delay = 0) => {
+                if (!context) return;
                 const sound = this.getSound(id);
                 if (sound) TweenManager.tween(sound.gain, { value: 0 }, time, ease, delay, () => sound.stop());
             };
 
             this.mute = () => {
+                if (!context) return;
                 TweenManager.tween(this.gain, { value: 0 }, 300, 'easeOutSine');
             };
 
             this.unmute = () => {
+                if (!context) return;
                 TweenManager.tween(this.gain, { value: 1 }, 500, 'easeOutSine');
             };
 
             this.stop = () => {
+                if (!context) return;
                 this.active = false;
                 for (let id in sounds) {
                     const sound = sounds[id];
