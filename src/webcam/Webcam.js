b3d28fb228cdac4b27e3ec04174fd5e3c42884ee
Patrick Schroen <ps@ufotechnologies.com>
2018-04-11 21:34:39 -0400

    Updated Webcam and WebcamMotion

--- src/webcam/Webcam.js
+++ src/webcam/Webcam.js
@@ -5,6 +5,7 @@
  */
 
 import { Events } from '../util/Events.js';
+import { Device } from '../util/Device.js';
 import { Component } from '../util/Component.js';
 import { Interface } from '../util/Interface.js';
 import { Canvas } from '../canvas/Canvas.js';
@@ -16,9 +17,12 @@ class Webcam extends Component {
     constructor(width, height, audio) {
         super();
         const self = this;
+        this.facing = 'back';
+        const cameras = {};
+        let back = false,
+            attempts = 0;
 
         createVideo();
-        initUserMedia();
 
         function createVideo() {
             self.element = document.createElement('video');
@@ -26,35 +30,122 @@ class Webcam extends Component {
             self.element.height = height;
             self.element.autoplay = true;
             self.object = new Interface(self.element);
+            self.width = width;
+            self.height = height;
+            self.object.size(self.width, self.height);
+            if (!Device.mobile) self.object.transform({ scaleX: -1 });
         }
 
-        function initUserMedia() {
-            navigator.getUserMedia({ video: true, audio }, success, error);
+        function establishWebcam() {
+            if (attempts >= 2) return error();
+            lookupDevices().then(() => {
+                if (self.stream && self.config.back) self.stream.getTracks()[0].stop();
+                navigator.getUserMedia({
+                    video: self.config.back ? cameras.back : cameras.front || true,
+                    audio
+                }, success, error);
+            });
+            attempts += 1;
+        }
+
+        function lookupDevices() {
+            const promise = Promise.create();
+            if (!Device.mobile) return Promise.resolve();
+            navigator.mediaDevices.enumerateDevices().then(devices => {
+                devices.forEach(device => {
+                    if (device.label.includes('front')) {
+                        cameras.front = {
+                            deviceId: { exact: device.deviceId }
+                        };
+                    }
+                    if (device.label.includes('back')) {
+                        cameras.back = {
+                            deviceId: { exact: device.deviceId }
+                        };
+                        back = true;
+                    }
+                });
+                if (!cameras.front) {
+                    cameras.front = {
+                        facingMode: 'user'
+                    };
+                }
+                if (!cameras.back) {
+                    cameras.back = {
+                        facingMode: 'environment'
+                    };
+                    back = false;
+                }
+                promise.resolve();
+            });
+            return promise;
         }
 
         function success(stream) {
-            self.element.src = URL.createObjectURL(stream);
-            self.events.fire(Events.READY, null, true);
+            self.denied = false;
+            self.stream = stream;
+            if (self.config.back && !back) {
+                establishWebcam();
+            } else {
+                self.element.src = URL.createObjectURL(stream);
+                self.events.fire(Events.READY, null, true);
+            }
         }
 
         function error() {
+            self.denied = true;
             self.events.fire(Events.ERROR, null, true);
         }
 
-        this.size = (w, h) => {
-            this.element.width = width = w;
-            this.element.height = height = h;
-            if (this.canvas) this.canvas.size(w, h);
+        this.createStream = config => {
+            attempts = 0;
+            this.config = config;
+            establishWebcam();
         };
 
-        this.getPixels = () => {
-            if (!this.canvas) this.canvas = this.initClass(Canvas, width, height);
-            this.canvas.context.drawImage(this.element, 0, 0, width, height);
-            return this.canvas.context.getImageData(0, 0, width, height);
+        this.flip = () => {
+            if (!back) return;
+            let direction;
+            if (this.facing === 'front') {
+                this.facing = 'back';
+                direction = cameras.back;
+            } else {
+                this.facing = 'front';
+                direction = cameras.front;
+            }
+            this.stream.getTracks()[0].stop();
+            navigator.getUserMedia({
+                video: direction || true,
+                audio
+            }, success, error);
+        };
+
+        this.size = (w, h) => {
+            this.element.width = this.width = w;
+            this.element.height = this.height = h;
+            this.object.size(this.width, this.height);
+        };
+
+        this.getPixels = (w = this.width, h = this.height) => {
+            if (!this.canvas) this.canvas = this.initClass(Canvas, w, h);
+            this.canvas.context.drawImage(this.element, 0, 0, w, h);
+            return this.canvas.context.getImageData(0, 0, w, h);
         };
 
         this.ready = () => {
             return this.element.readyState > 0;
+        };
+
+        this.end = () => {
+            this.active = false;
+            this.element.pause();
+            if (this.stream) this.stream.getTracks()[0].enabled = false;
+        };
+
+        this.restart = () => {
+            this.element.play();
+            if (this.stream) this.stream.getTracks()[0].enabled = true;
+            this.active = true;
         };
 
         this.destroy = () => {
--- src/webcam/WebcamMotion.js
+++ src/webcam/WebcamMotion.js
@@ -1,5 +1,5 @@
 /**
- * Webcam interaction.
+ * Webcam motion tracker.
  *
  * @author Patrick Schroen / https://github.com/pschroen
  */
@@ -7,19 +7,13 @@
 import { Utils } from '../util/Utils.js';
 import { Events } from '../util/Events.js';
 import { Component } from '../util/Component.js';
-import { Stage } from '../view/Stage.js';
-import { Webcam } from './Webcam.js';
 
 class WebcamMotion extends Component {
 
-    static instance() {
-        if (!this.singleton) this.singleton = new WebcamMotion();
-        return this.singleton;
-    }
-
-    constructor(width = 64, height = 48, threshold = 80, length = 50, fps = 24) {
+    constructor(webcam, width = 64, height = 48, threshold = 80, length = 50, fps = 24) {
         super();
         const self = this;
+        this.capturing = false;
         const gesture = {
                 xDir: null,
                 xValue: 0,
@@ -30,38 +24,20 @@ class WebcamMotion extends Component {
             average = {},
             lastAverage = {},
             event = {},
-            callbacks = [],
             outputAverage = {};
-        let webcam, oldPixels, first, outputMotion;
+        let oldPixels, outputMotion;
 
-        if (!navigator.getUserMedia) return;
-        initCamera();
-
-        function initCamera() {
-            webcam = self.initClass(Webcam, width, height, false);
-            webcam.events.add(Events.READY, webcamReady);
-            webcam.object.css({
-                position: 'absolute',
-                right: '100%'
-            });
-            Stage.add(webcam);
-        }
-
-        function webcamReady() {
-            self.activated = true;
-        }
+        this.startRender(loop, fps);
 
         function loop() {
+            if (!self.capturing) return;
             process();
             track();
         }
 
         function process() {
-            const pixels = webcam.getPixels().data;
-            if (!first) {
-                oldPixels = pixels;
-                first = true;
-            }
+            const pixels = webcam.getPixels(width, height).data;
+            if (!oldPixels) return oldPixels = pixels;
             motion.length = 0;
             for (let i = 0; i < height; i++) {
                 for (let j = 0; j < width; j++) {
@@ -98,54 +74,35 @@ class WebcamMotion extends Component {
             });
             event.average = outputAverage;
             event.motion = outputMotion;
-            fire('motion', event);
-            if (lastAverage) {
-                const diffX = lastAverage.x - average.x,
-                    xDir = diffX < 0 ? 'left' : 'right';
-                if (xDir !== gesture.xDir) {
-                    gesture.xDir = xDir;
-                    gesture.xValue = Math.abs(diffX);
-                }
-                gesture.xValue += Math.abs(diffX);
-                if (gesture.xValue > 20) {
-                    fire('gesture', gesture.xDir);
-                    gesture.xValue = 0;
-                }
-                const diffY = lastAverage.y - average.y,
-                    yDir = diffY < 0 ? 'down' : 'up';
-                if (yDir !== gesture.yDir) {
-                    gesture.yDir = yDir;
-                    gesture.yValue = Math.abs(diffY);
-                }
-                gesture.yValue += Math.abs(diffY);
-                if (gesture.yValue > 20) {
-                    fire('gesture', gesture.yDir);
-                    gesture.yValue = 0;
-                }
+            event.gesture = null;
+            self.events.fire(Events.UPDATE, event, true);
+            const diffX = lastAverage.x - average.x,
+                xDir = diffX < 0 ? 'left' : 'right';
+            if (xDir !== gesture.xDir) {
+                gesture.xDir = xDir;
+                gesture.xValue = Math.abs(diffX);
+            }
+            gesture.xValue += Math.abs(diffX);
+            if (gesture.xValue > 20) {
+                event.gesture = gesture.xDir;
+                self.events.fire(Events.UPDATE, event, true);
+                gesture.xValue = 0;
+            }
+            const diffY = lastAverage.y - average.y,
+                yDir = diffY < 0 ? 'down' : 'up';
+            if (yDir !== gesture.yDir) {
+                gesture.yDir = yDir;
+                gesture.yValue = Math.abs(diffY);
+            }
+            gesture.yValue += Math.abs(diffY);
+            if (gesture.yValue > 20) {
+                event.gesture = gesture.yDir;
+                self.events.fire(Events.UPDATE, event, true);
+                gesture.yValue = 0;
             }
             lastAverage.x = average.x;
             lastAverage.y = average.y;
         }
-
-        function fire(event, data) {
-            for (let i = 0; i < callbacks.length; i++) if (callbacks[i].event === event) callbacks[i].callback(data);
-        }
-
-        this.start = () => {
-            this.startRender(loop, fps);
-        };
-
-        this.stop = () => {
-            this.stopRender(loop);
-        };
-
-        this.bind = (event, callback) => {
-            callbacks.push({ event, callback });
-        };
-
-        this.unbind = (event, callback) => {
-            for (let i = 0; i < callbacks.length; i++) if (callbacks[i].event === event && callbacks[i].callback === callback) callbacks.splice(i, 1)[0] = null;
-        };
     }
 }
 
