036c71c613f1a3419ebdafb5be0079a440b6f335
Patrick Schroen <ps@ufotechnologies.com>
2019-01-07 21:28:08 -0500

    Updated Interaction3D, ShaderInteraction3D and ShaderInteraction2D

--- src/three/Interaction3D.js
+++ src/three/Interaction3D.js
@@ -4,7 +4,11 @@
  * @author Patrick Schroen / https://github.com/pschroen
  */
 
+import * as THREE from 'three';
+
 import { Render } from '../util/Render.js';
+import { Events } from '../util/Events.js';
+import { Device } from '../util/Device.js';
 import { Component } from '../util/Component.js';
 import { Interaction } from '../util/Interaction.js';
 import { Mouse } from '../util/Mouse.js';
@@ -19,25 +23,69 @@ class Interaction3D extends Component {
             Interaction3D.HOVER = 'interaction3d_hover';
             Interaction3D.CLICK = 'interaction3d_click';
 
+            const map = new Map();
+            let input = Mouse;
+
+            Interaction3D.find = camera => {
+                if (!map.has(camera)) {
+                    const interaction = new Interaction3D(camera);
+                    interaction.input(input);
+                    map.set(camera, interaction);
+                }
+                return map.get(camera);
+            };
+
+            Interaction3D.useInput = v => {
+                for (let [interaction] of map) interaction.input(v);
+                input = v;
+            };
+
             Interaction3D.initialized = true;
         }
 
         super();
         const self = this;
-        const event = {};
-        let hoverTarget, clickTarget;
+        const test = [],
+            event = {},
+            v3 = new THREE.Vector3();
+        let hoverTarget, clickTarget, input;
 
-        this.ray = new Raycaster(camera);
+        this.ray = this.initClass(Raycaster, camera);
         this.meshes = [];
         this.callbacks = [];
         this.enabled = true;
 
-        addListeners();
+        function canTest(mesh) {
+            if (!mesh.visible) return false;
+            let parent = mesh.parent;
+            while (parent) {
+                if (!parent.visible) return false;
+                parent = parent.parent;
+            }
+            return true;
+        }
+
+        function testObjects() {
+            test.length = 0;
+            for (let i = self.meshes.length - 1; i >= 0; i--) {
+                const mesh = self.meshes[i];
+                if (canTest(mesh)) test.push(mesh);
+            }
+            return test;
+        }
 
         function addListeners() {
             self.events.add(Mouse.input, Interaction.START, start);
             self.events.add(Mouse.input, Interaction.MOVE, move);
             self.events.add(Mouse.input, Interaction.CLICK, click);
+            if (Device.mobile) self.events.add(Mouse.input, Interaction.END, end);
+        }
+
+        function removeListeners() {
+            self.events.remove(Mouse.input, Interaction.START, start);
+            self.events.remove(Mouse.input, Interaction.MOVE, move);
+            self.events.remove(Mouse.input, Interaction.CLICK, click);
+            if (Device.mobile) self.events.remove(Mouse.input, Interaction.END, end);
         }
 
         function start() {
@@ -53,76 +101,87 @@ class Interaction3D extends Component {
 
         function move() {
             if (!self.enabled) return;
-            const hit = self.ray.checkHit(self.meshes);
-            if (hit[0]) {
-                const mesh = hit[0].object;
-                if (mesh !== hoverTarget) {
-                    if (hoverTarget) triggerHover('out', hoverTarget);
-                    hoverTarget = mesh;
-                    triggerHover('over', hoverTarget);
-                    Stage.css('cursor', 'pointer');
-                }
-                return hit[0];
+            let hit;
+            if (input.type === '3d') {
+                v3.set(0, 0, -1).applyQuaternion(input.quaternion);
+                hit = self.ray.checkFromValues(testObjects(), input.position, v3)[0];
             } else {
-                if (hoverTarget) {
-                    triggerHover('out', hoverTarget);
-                    hoverTarget = null;
-                    Stage.css('cursor', '');
+                hit = self.ray.checkHit(testObjects(), input.position)[0];
+            }
+            if (hit) {
+                const mesh = hit.object;
+                if (input.object && input.object.setHitPosition) input.object.setHitPosition(hit);
+                if (mesh !== hoverTarget) {
+                    if (hoverTarget) triggerHover('out', hoverTarget, hit);
+                    hoverTarget = mesh;
+                    triggerHover('over', hoverTarget, hit);
+                    Stage.css('cursor', self.callbacks[self.meshes.indexOf(hoverTarget)].clickCallback ? 'pointer' : '');
                 }
+                return hit;
+            } else {
+                end();
+                if (input.object && input.object.setHitPosition) input.object.setHitPosition(false);
                 return false;
             }
         }
 
-        function click() {
+        function end() {
             if (!self.enabled) return;
-            if (!clickTarget) return;
-            const hit = self.ray.checkHit(self.meshes)[0];
-            if (hit && hit.object === clickTarget) triggerClick(clickTarget);
-            clickTarget = null;
+            if (hoverTarget) {
+                triggerHover('out', hoverTarget, null);
+                hoverTarget = null;
+                Stage.css('cursor', '');
+            }
         }
 
-        function triggerHover(action, mesh) {
+        function click(e) {
+            if (!self.enabled) return;
+            if (!clickTarget) return;
+            let hit;
+            if (input.type === '3d') {
+                v3.set(0, 0, -1).applyQuaternion(input.quaternion);
+                hit = self.ray.checkFromValues(testObjects(), input.position, v3)[0];
+            } else {
+                const element = document.elementFromPoint(e.x, e.y);
+                if (element && element.className === 'hit') return;
+                hit = self.ray.checkHit(testObjects(), input.position)[0];
+            }
+            if (hit && hit.object === clickTarget) {
+                triggerClick(clickTarget, hit);
+                clickTarget = null;
+            }
+        }
+
+        function triggerHover(action, mesh, hit) {
             event.action = action;
             event.mesh = mesh;
+            event.hit = hit;
             self.events.fire(Interaction3D.HOVER, event, true);
             const i = self.meshes.indexOf(hoverTarget);
             if (self.callbacks[i].overCallback) self.callbacks[i].overCallback(event);
         }
 
-        function triggerClick(mesh) {
+        function triggerClick(mesh, hit) {
             event.action = 'click';
             event.mesh = mesh;
+            event.hit = hit;
             self.events.fire(Interaction3D.CLICK, event, true);
             const i = self.meshes.indexOf(clickTarget);
             if (self.callbacks[i].clickCallback) self.callbacks[i].clickCallback(event);
         }
 
-        function parseMeshes(meshes) {
+        this.add = (meshes, overCallback, clickCallback) => {
             if (!Array.isArray(meshes)) meshes = [meshes];
-            const output = [];
-
-            function checkMesh(mesh) {
-                if (mesh.type === 'Mesh' && mesh.mouseEnabled) output.push(mesh);
-                if (mesh.children.length) mesh.children.forEach(checkMesh);
-            }
-
-            meshes.forEach(checkMesh);
-            return output;
-        }
-
-        this.add = (meshes, overCallback, clickCallback, parse) => {
-            if (!Array.isArray(meshes) || parse) meshes = parseMeshes(meshes);
             meshes.forEach(mesh => {
                 this.meshes.push(mesh);
                 this.callbacks.push({ overCallback, clickCallback });
             });
         };
 
-        this.remove = (meshes, parse) => {
-            if (!Array.isArray(meshes) || parse) meshes = parseMeshes(meshes);
+        this.remove = meshes => {
+            if (!Array.isArray(meshes)) meshes = [meshes];
             meshes.forEach(mesh => {
                 if (mesh === hoverTarget) {
-                    triggerHover('out', hoverTarget);
                     hoverTarget = null;
                     Stage.css('cursor', '');
                 }
@@ -132,6 +191,22 @@ class Interaction3D extends Component {
                     this.callbacks.splice(i, 1);
                 }
             });
+        };
+
+        this.input = object => {
+            input = {};
+            input.object = object;
+            input.position = object.group && object.group.position || object;
+            input.quaternion = object.group && object.group.quaternion;
+            input.type = typeof input.position.z === 'number' ? '3d' : '2d';
+            if (object === Mouse) {
+                addListeners();
+            } else {
+                removeListeners();
+                this.events.add(object, Events.SELECT, start);
+                this.events.add(object, Events.END, click);
+                this.startRender(move, 24);
+            }
         };
     }
 }
